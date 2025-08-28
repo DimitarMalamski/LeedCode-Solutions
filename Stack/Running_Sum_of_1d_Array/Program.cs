2afcefcc7e020e0b94a850581d6f1131bedb9ea3
DimitarMalamski <mitemalamata@gmail.com>
2025-08-28 22:10:00 +0200

    add solution for Running Sum of 1d Array

---
+++ Stack/Running_Sum_of_1d_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 1480. Running Sum of 1d Array
+// Link https://leetcode.com/problems/running-sum-of-1d-array/description/
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = [1, 2, 3, 4];
+      int[] output = Check(nums);
+      Console.WriteLine(string.Join(", ", output));
+   }
+   static int[] Check(int[] nums)
+   {
+      int[] result = new int[nums.Length];
+
+      int sum = 0;
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+         sum += nums[i];
+         result[i] = sum;
+      }
+
+      return result;
+   }
+}
