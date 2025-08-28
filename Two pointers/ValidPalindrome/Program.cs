40e16ee59a630354f1e2e3177c920117362915ec
DimitarMalamski <mitemalamata@gmail.com>
2025-08-28 21:26:03 +0200

    add solution for Valid Palindrome ( Two pointers )

---
+++ Two pointers/ValidPalindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿// Problem 125. Valid Palindrome
+// Link https://leetcode.com/problems/valid-palindrome/description/
+// Difficulty: Easy
+
+using System;
+using System.Linq;
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string input = "Was it a car or a cat I saw?";
+      bool output = Check(input);
+      Console.WriteLine(output);
+   }
+   static bool Check(string input)
+   {
+      string filtered = new string(input.Where(char.IsLetterOrDigit)
+                                       .Select(char.ToLowerInvariant)
+                                       .ToArray());
+
+      for (int i = 0; i < filtered.Length; i++) {
+         if (filtered[i] != filtered[(filtered.Length - 1) - i]) {
+            return false;
+         }
+      }
+
+      return true;
+   }
+}
