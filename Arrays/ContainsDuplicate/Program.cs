b321220fc0c879a35b5709ec29beb226df3a1dd2
DimitarMalamski <mitemalamata@gmail.com>
2025-08-28 02:59:25 +0200

    add solution for Contains Duplicate

---
+++ Arrays/ContainsDuplicate/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem 217. Contains Duplicate
+// Link https://leetcode.com/problems/contains-duplicate/description/
+// Difficulty: Easy
+
+class Program {
+   static void Main(string[] args) {
+      int[] nums = { 1, 2, 3, 3 };
+      bool output = Solution(nums);
+      Console.WriteLine(output);
+   }
+   static bool Solution(int[] nums)
+   {
+      HashSet<int> set = new HashSet<int>();
+
+      foreach (int num in nums)
+      {
+         if (set.Contains(num))
+         {
+            return true;
+         }
+         set.Add(num);
+      }
+
+      return false;
+   }
+}
