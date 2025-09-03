25441abe3117332fa551e0ea5f1e28a0da793d75
DimitarMalamski <mitemalamata@gmail.com>
2025-09-03 14:18:15 +0200

    add solution for Number of Steps Reduce a Number To Zero

---
+++ Math/NumberofStepstoReduceaNumbertoZero/Program.cs
@@ -0,0 +1,33 @@
+﻿// Problem 1342. Number of Steps to Reduce a Number to Zero
+// Link https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int input = 123;
+      int output = Check(input);
+      Console.WriteLine(output);
+   }
+   static int Check(int input)
+   {
+      int count = 0;
+
+      while (input != 0)
+      {
+         if (input % 2 == 0)
+         {
+            input /= 2;
+            count++;
+         }
+         else
+         {
+            input -= 1;
+            count++;
+         }
+      }
+
+      return count;
+   }
+}
