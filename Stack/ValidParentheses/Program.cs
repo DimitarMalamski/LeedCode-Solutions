31e8c03d9fd245ddf663ef407307b67e3918786c
DimitarMalamski <mitemalamata@gmail.com>
2025-09-07 20:49:39 +0200

    add solution for Valid Parentheses ( Stack )

---
+++ Stack/ValidParentheses/Program.cs
@@ -0,0 +1,45 @@
+﻿// Problem 20. Valid Parentheses
+// Link https://leetcode.com/problems/valid-parentheses/description/
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string input = "([])";
+      bool output = SolutionOne(input);
+      Console.WriteLine(output);
+   }
+   static bool SolutionOne(string input)
+   {
+      if (input.Length < 2)
+      {
+         return false;
+      }
+
+      Stack<char> stack = new Stack<char>();
+
+      foreach (char item in input)
+      {
+         switch (item)
+         {
+            case '(':
+            case '{':
+            case '[':
+               stack.Push(item);
+               break;
+            case ')':
+               if (stack.Count == 0 || stack.Pop() != '(') return false;
+               break;
+            case '}':
+               if (stack.Count == 0 || stack.Pop() != '{') return false;
+               break;
+            case ']':
+               if (stack.Count == 0 || stack.Pop() != '[') return false;
+               break;
+         }
+      }
+
+      return stack.Count == 0;
+   }
+}
