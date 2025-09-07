1714146c7253a5aa48153c03c631a3f4314ecc47
DimitarMalamski <mitemalamata@gmail.com>
2025-09-07 19:41:55 +0200

    add solution for Best Time To Buy And Sell Stock ( Arrays )

---
+++ Arrays/BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,34 @@
+﻿// Problem 121. Best Time to Buy and Sell Stock
+// Link https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] input = new int[] { 10, 1, 5, 6, 7, 1 };
+      int output = SolutionOne(input);
+      Console.WriteLine(output);
+   }
+
+   static int SolutionOne(int[] input)
+   {
+      int minPrice = int.MaxValue;
+      int maxProfit = 0;
+
+      foreach (int price in input)
+      {
+         if (price < minPrice)
+         {
+            minPrice = price;
+         }
+
+         int currentProfit = price - minPrice;
+         if (currentProfit > maxProfit)
+         {
+            maxProfit = currentProfit;
+         }
+      }
+
+      return maxProfit;
+   }
+}
