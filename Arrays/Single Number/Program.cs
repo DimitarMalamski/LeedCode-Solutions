048a3e973c721f48faf89066d22a621ff81276f6
DimitarMalamski <mitemalamata@gmail.com>
2025-09-12 15:57:01 +0200

    add solution for Singe Number ( Arrays )

---
+++ Arrays/Single Number/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem 136. Single Number
+// Link https://leetcode.com/problems/single-number/description/?envType=problem-list-v2&envId=array
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] input = { 4,1 ,2,1,2 };
+      int output = SingleNumber(input);
+      Console.WriteLine(output);
+   }
+   static int SingleNumber(int[] input)
+   {
+      Dictionary<int, int> map = new Dictionary<int, int>();
+
+      foreach (int num in input)
+      {
+         if (!map.ContainsKey(num))
+         {
+            map[num] = 0;
+         }
+
+         map[num]++;
+      }
+
+      return map.FirstOrDefault(x => x.Value == 1).Key;
+   }
+}
