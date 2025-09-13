e4fc9876e08e76575a38435f04e6dc0e3dc2ee71
DimitarMalamski <mitemalamata@gmail.com>
2025-09-13 19:16:11 +0200

    add solution for Missing Number ( Arrays )

---
+++ Arrays/Missing Number/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem 268. Missing Number
+// Link https://leetcode.com/problems/missing-number/description/?envType=problem-list-v2&envId=array
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] input = { 0, 1 };
+      int output = MissingNumber(input);
+      Console.WriteLine(output);
+   }
+   static int MissingNumber(int[] nums)
+   {
+      Array.Sort(nums);
+        
+      if (nums[0] != 0) {
+         return 0;
+      }
+
+      for (int i = 0; i < nums.Length - 1; i++) {
+         if (nums[i] + 1 != nums[i + 1]) {
+            return nums[i] += 1;
+         }
+      }
+
+      return nums.Length;
+   }
+}
