093189aa822d6579007ff63dee18972f2201b587
DimitarMalamski <mitemalamata@gmail.com>
2025-09-13 19:23:37 +0200

    optimize solution for Missing Number ( Arrays )

--- Arrays/Missing Number/Program.cs
+++ Arrays/Missing Number/Program.cs
@@ -6,23 +6,38 @@ class Solution
    static void Main(string[] args)
    {
       int[] input = { 0, 1 };
-      int output = MissingNumber(input);
+      int output = MissingNumberOptimized(input);
       Console.WriteLine(output);
    }
    static int MissingNumber(int[] nums)
    {
       Array.Sort(nums);
-        
-      if (nums[0] != 0) {
+
+      if (nums[0] != 0)
+      {
          return 0;
       }
 
-      for (int i = 0; i < nums.Length - 1; i++) {
-         if (nums[i] + 1 != nums[i + 1]) {
+      for (int i = 0; i < nums.Length - 1; i++)
+      {
+         if (nums[i] + 1 != nums[i + 1])
+         {
             return nums[i] += 1;
          }
       }
 
       return nums.Length;
+   }
+   static int MissingNumberOptimized(int[] nums)
+   {
+      int expectedSum = nums.Length * (nums.Length + 1) / 2;
+      int actualSum = 0;
+
+      foreach (int num in nums)
+      {
+         actualSum += num;
+      }
+
+      return expectedSum - actualSum;
    }
 }
