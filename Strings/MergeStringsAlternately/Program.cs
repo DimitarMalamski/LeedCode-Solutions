32a9148d17cef9987f48e3b3a21d53fbca150272
DimitarMalamski <mitemalamata@gmail.com>
2025-09-14 19:21:53 +0200

    add solution for Merge Strings Alternately ( String )

---
+++ Strings/MergeStringsAlternately/Program.cs
@@ -0,0 +1,68 @@
+﻿// Problem 1768. Merge Strings Alternately
+// Link https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string word1 = "ab";
+      string word2 = "pqrs";
+      string output = MergeAlternately(word1, word2);
+
+      Console.WriteLine(output);
+   }
+   static string MergeAlternately(string word1, string word2)
+   {
+      if (word1 == "")
+      {
+         return word2;
+      }
+
+      if (word2 == "")
+      {
+         return word1;
+      }
+
+      int pointerOne = 0;
+      int pointerTwo = 0;
+      string output = "";
+
+      if (word1.Length >= word2.Length)
+      {
+         while (pointerTwo != word2.Length)
+         {
+            output += word1[pointerOne];
+            output += word2[pointerTwo];
+
+            pointerOne++;
+            pointerTwo++;
+         }
+
+         while (pointerOne != word1.Length)
+         {
+            output += word1[pointerOne];
+            pointerOne++;
+         }
+      }
+      else
+      {
+         while (pointerOne != word1.Length)
+         {
+            output += word1[pointerOne];
+            output += word2[pointerTwo];
+
+            pointerOne++;
+            pointerTwo++;
+         }
+
+         while (pointerTwo != word2.Length)
+         {
+            output += word2[pointerTwo];
+            pointerTwo++;
+         }
+      }
+
+      return output;
+   }
+}
