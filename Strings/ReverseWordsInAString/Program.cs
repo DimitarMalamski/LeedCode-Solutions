5f467aaa2d1d7baa765758d5c9e860064238c906
DimitarMalamski <mitemalamata@gmail.com>
2025-09-19 13:29:44 +0200

    add solution for Reverse Words In A String ( Strings )

---
+++ Strings/ReverseWordsInAString/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 151. Reverse Words in a String
+// Link https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=leetcode-75
+// Difficulty: Medium
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string s = "the sky is blue";
+      string output = ReverseWords(s);
+      Console.WriteLine(output);
+   }
+   static string ReverseWords(string s)
+   {
+      var parts = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+      Array.Reverse(parts);
+
+      return string.Join(' ', parts);
+   } 
+}
+
+
+
