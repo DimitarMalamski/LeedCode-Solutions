dbea7d0aa03128f88cb0b8e23919ee18f5b7963f
DimitarMalamski <mitemalamata@gmail.com>
2025-10-05 22:36:48 +0200

    add solution for Find The Difference ( Strings )

---
+++ Strings/FindTheDifference/Program.cs
@@ -0,0 +1,29 @@
+﻿// Problem 389. Find the Difference
+// Link https://leetcode.com/problems/find-the-difference/description/?envType=study-plan-v2&envId=programming-skills
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string s = "abcd";
+      string t = "abcde";
+      char output = FindTheDifference(s, t);
+      Console.WriteLine(output);
+   }
+   static char FindTheDifference(string s, string t)
+   {
+      int[] set = new int[26];
+
+      foreach (char c in s)
+      {
+         set[c - 'a']++;
+      }
+
+      foreach (char c in t)
+      {
+         if (--set[c - 'a'] < 0) return c;
+      }
+
+      return ' ';
+   }
+}
