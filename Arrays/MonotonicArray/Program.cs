9c00274ee1a7dd5a6421670e1134cc27732bbd16
DimitarMalamski <mitemalamata@gmail.com>
2025-10-09 17:04:38 +0200

    add solution for Monotonic Array ( Array )

---
+++ Arrays/MonotonicArray/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem 896. Monotonic Array
+// Link https://leetcode.com/problems/monotonic-array/description/?envType=study-plan-v2&envId=programming-skills
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 1, 2, 4, 3 };
+      bool output = IsMonotonic(nums);
+      Console.WriteLine(output);
+   }
+   static bool IsMonotonic(int[] nums)
+   {
+      bool up = true;
+      bool down = true;
+
+      for (int i = 1; i < nums.Length; i++)
+      {
+         if (nums[i] > nums[i - 1]) down = false;
+         if (nums[i] < nums[i - 1]) up = false;
+      }
+
+      return up || down;
+      
+   }
+}
