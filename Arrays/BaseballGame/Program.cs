6ad0fb96c56b41c4d5364ecf1a393643489844f6
DimitarMalamski <mitemalamata@gmail.com>
2025-10-13 14:24:48 +0200

    add solution for Baseball Game ( Arrays )

---
+++ Arrays/BaseballGame/Program.cs
@@ -0,0 +1,60 @@
+﻿// Problem 682. Baseball Game
+// Link https://leetcode.com/problems/baseball-game/description/?envType=study-plan-v2&envId=programming-skills
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string[] operations = { "5","-2","4","C","D","9","+","+" };
+      int output = CalPoints(operations);
+      Console.WriteLine(output);
+   }
+   static int CalPoints(string[] operations)
+   {
+      // "5", "2", "C", "D", "+"
+
+      Stack<int> stack = new Stack<int>();
+
+      for (int i = 0; i < operations.Length; i++)
+      {
+         if (int.TryParse(operations[i], out int n))
+         {
+            stack.Push(n);
+         }
+         else
+         {
+            switch (char.Parse(operations[i]))
+            {
+               case '+':
+                  int numberOne = stack.Pop();
+                  int numberTwo = stack.Pop();
+
+                  int sumTwo = numberOne + numberTwo;
+
+                  stack.Push(numberTwo);
+                  stack.Push(numberOne);
+                  stack.Push(sumTwo);
+                  break;
+               case 'D':
+                  int current = stack.Pop();
+                  int product = current * 2;
+                  stack.Push(current);
+                  stack.Push(product);
+                  break;
+               case 'C':
+                  stack.Pop();
+                  break;
+            }
+         }
+      }
+
+      int sum = 0;
+      while (stack.Count > 0)
+      {
+         sum += stack.Pop();
+      }
+
+      return sum;
+   }
+}
