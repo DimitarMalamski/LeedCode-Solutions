04bc3f0ba636eb505d4c1eba747c54eea5a43feb
DimitarMalamski <mitemalamata@gmail.com>
2025-10-14 16:23:26 +0200

    add solution for Robot Return to Origin ( Strings )

---
+++ Strings/RobotReturnToOrigin/Program.cs
@@ -0,0 +1,40 @@
+﻿// Problem 657. Robot Return to Origin
+// Link https://leetcode.com/problems/robot-return-to-origin/description/?envType=study-plan-v2&envId=programming-skills
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string moves = "LL";
+      bool output = JudgeCircle(moves);
+      Console.WriteLine(output);
+   }
+   static bool JudgeCircle(string moves)
+   {
+      char[] chars = moves.ToCharArray();
+      int x = 0;
+      int y = 0;
+
+      foreach (char move in chars)
+      {
+         switch (move)
+         {
+            case 'U':
+               y++;
+               break;
+            case 'D':
+               y--;
+               break;
+            case 'L':
+               x--;
+               break;
+            case 'R':
+               x++;
+               break;
+         }
+      }
+
+      return x == 0 && y == 0;
+   }
+}
+
