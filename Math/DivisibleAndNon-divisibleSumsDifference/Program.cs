eb82698635722bcd5f8463ec6ff3b9969e0cc9e4
DimitarMalamski <mitemalamata@gmail.com>
2025-10-23 10:33:13 +0200

    add solution for Divisible and Non-divisible Sums Difference ( Math )

---
+++ Math/DivisibleAndNon-divisibleSumsDifference/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem 2894. Divisible and Non-divisible Sums Difference
+// Link https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 5, m = 1;
+      int output = DifferenceOfSums(n, m);
+      Console.WriteLine(output);
+   }
+   static int DifferenceOfSums(int n, int m)
+   {
+      int num1 = 0;
+      int num2 = 0;
+
+      for (int i = 1; i <= n; i++)
+      {
+         if (i % m == 0)
+         {
+            num2 += i;
+         }
+         else
+         {
+            num1 += i;
+         }
+      }
+
+      return num1 - num2;
+   }
+}
