768bc707dff4a5dcc4ef585b44bd2a028481cc56
DimitarMalamski <mitemalamata@gmail.com>
2025-10-23 10:36:58 +0200

    optimize solution for Divisible and Non-divisible Sums Difference ( Math )

--- Math/DivisibleAndNon-divisibleSumsDifference/Program.cs
+++ Math/DivisibleAndNon-divisibleSumsDifference/Program.cs
@@ -28,4 +28,12 @@ class Solution
 
       return num1 - num2;
    }
+   static int DifferenceOfSumsOptimized(int n, int m)
+   {
+      int totalSum = n * (n + 1) / 2;
+      int k = n / m;
+      int divisibleSum = m * (k * (k + 1)) / 2;
+
+      return totalSum - 2 * divisibleSum;
+   }
 }
