e7eec1ed3aa3e16f4742d7ab8df4f354ca157f93
DimitarMalamski <mitemalamata@gmail.com>
2025-10-23 10:24:13 +0200

    add solution for Score of a String ( Strings )

---
+++ Strings/ScoreOfAString/Program.cs
@@ -0,0 +1,25 @@
+﻿// Problem 3110. Score of a String
+// Link https://leetcode.com/problems/score-of-a-string/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string s = "hello";
+      int output = ScoreOfString(s);
+      Console.WriteLine(output);
+   }
+   static int ScoreOfString(string s)
+   {
+      int sum = 0;
+      char[] chars = s.ToCharArray();
+
+      for (int i = 1; i < chars.Length; i++)
+      {
+         int val = Math.Abs(chars[i] - chars[i - 1]);
+         sum += val;
+      }
+
+      return sum;
+   }
+}
