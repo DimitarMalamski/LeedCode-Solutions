58ec64690ac3bf216ebdcc2186b3f99706628db3
DimitarMalamski <mitemalamata@gmail.com>
2025-10-26 18:19:01 +0100

    add solution for Jewels and Stones ( Strings )

---
+++ Strings/JewelsAndStones/Program.cs
@@ -0,0 +1,30 @@
+﻿// Problem 771. Jewels and Stones
+// Link https://leetcode.com/problems/jewels-and-stones/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string jewels = "aA";
+      string stones = "aAAbbbb";
+      int output = NumJewelsInStones(jewels, stones);
+      Console.WriteLine(output);
+   }
+   static int NumJewelsInStones(string jewels, string stones)
+   {
+      int count = 0;
+
+      char[] chars = jewels.ToCharArray();
+      char[] stoneChars = stones.ToCharArray();
+
+      foreach (var stone in stoneChars)
+      {
+         if (chars.Contains(stone))
+         {
+            count++;
+         }
+      }
+
+      return count;
+   }
+}
