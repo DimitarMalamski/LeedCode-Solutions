a1abb95a82fd28e91a3dc22ba891eb0b524bd3b4
DimitarMalamski <mitemalamata@gmail.com>
2025-10-26 18:48:29 +0100

    add solution for Number of Employees Who Met the Target ( Arrays )

---
+++ Arrays/NumberOfEmployeesWhoMetTheTarget/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 2798. Number of Employees Who Met the Target
+// Link https://leetcode.com/problems/number-of-employees-who-met-the-target/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] hours = { 0, 1, 2, 3, 4 };
+      int target = 2;
+      int output = NumberOfEmployeesWhoMetTarget(hours, target);
+      Console.WriteLine(output);
+   }
+
+   static int NumberOfEmployeesWhoMetTarget(int hours, int target)
+   {
+      int count = 0;
+      foreach (int hour in hours)
+      {
+         if (hour >= target)
+         {
+            count++;
+         }
+      }
+
+      return count;
+   }
+}
