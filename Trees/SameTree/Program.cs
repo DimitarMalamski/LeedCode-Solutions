cadba4ae677f99870189f4a643c13e58e3f09563
DimitarMalamski <mitemalamata@gmail.com>
2025-10-28 12:08:13 +0100

    add solution for Same Tree ( Trees )

--- Trees/SameTree/Program.cs
+++ Trees/SameTree/Program.cs
@@ -13,9 +13,9 @@ class Solution
 
       TreeNode q = new TreeNode(1);
       q.left = new TreeNode(2);
-      q.right = new TreeNode(4);
+      q.right = new TreeNode(3);
 
-      bool output = sol.IsSameTree(p, q);
+      bool output = sol.IsSameTreeQueue(p, q);
       Console.WriteLine(output);
    }
    public bool IsSameTree(TreeNode p, TreeNode q)
@@ -36,6 +36,49 @@ class Solution
       }
 
       return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+   }
+
+   public bool IsSameTreeQueue(TreeNode p, TreeNode q)
+   {
+      if (p == null && q == null)
+         return true;
+      if (p == null || q == null)
+         return false;
+
+      Queue<TreeNode> queueP = new Queue<TreeNode>();
+      Queue<TreeNode> queueQ = new Queue<TreeNode>();
+
+      queueP.Enqueue(p);
+      queueQ.Enqueue(q);
+
+      while (queueP.Count > 0 && queueQ.Count > 0)
+      {
+         TreeNode nodeP = queueP.Dequeue();
+         TreeNode nodeQ = queueQ.Dequeue();
+
+         if (nodeP.val != nodeQ.val)
+         {
+            return false;
+         }
+
+         if ((nodeP.left == null) != (nodeQ.left == null)) return false;
+
+         if (nodeP.left != null)
+         {
+            queueP.Enqueue(nodeP.left);
+            queueQ.Enqueue(nodeQ.left!);
+         }
+
+         if ((nodeP.right == null) != (nodeQ.right == null)) return false;
+
+         if (nodeP.right != null)
+         {
+            queueP.Enqueue(nodeP.right);
+            queueQ.Enqueue(nodeQ.right!);
+         }
+      }
+
+      return queueP.Count == queueQ.Count;
    }
 
    public class TreeNode
