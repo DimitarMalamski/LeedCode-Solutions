36769f1cb3eb4e9450fef557e78fee08fcb98443
DimitarMalamski <mitemalamata@gmail.com>
2025-10-29 15:43:20 +0100

    add solution for Symmetric Tree ( Trees )

--- Trees/SymmetricTree/Program.cs
+++ Trees/SymmetricTree/Program.cs
@@ -1,4 +1,4 @@
-﻿// Problem 101. Symmetric Tree 
+﻿// Problem 101. Symmetric Tree
 // Link https://leetcode.com/problems/same-tree/description 
 // Difficulty: Easy
 
@@ -18,7 +18,7 @@ class Solution
       root.right.left = new TreeNode(4);
       root.right.right = new TreeNode(3);
 
-      bool output = sol.IsSymmetric(root);
+      bool output = sol.IsSymmetricSolutionTwo(root);
       Console.WriteLine(output);
    }
 
@@ -37,6 +37,31 @@ class Solution
          && IsMirror(t1.left, t2.right)
          && IsMirror(t1.right, t2.left);
    }
+
+   public bool IsSymmetricSolutionTwo(TreeNode root)
+   {
+      if (root == null) return true;
+
+      Queue<TreeNode> q = new Queue<TreeNode>();
+      q.Enqueue(root.left);
+      q.Enqueue(root.right);
+
+      while (q.Count > 0)
+      {
+         TreeNode t1 = q.Dequeue();
+         TreeNode t2 = q.Dequeue();
+
+         if (t1 == null && t2 == null) continue;
+         if (t1 == null || t2 == null || t1.val != t2.val) return false;
+
+         q.Enqueue(t1.left);
+         q.Enqueue(t2.right);
+         q.Enqueue(t1.right);
+         q.Enqueue(t2.left);
+      }
+
+      return true;
+   } 
    
    public class TreeNode
    {
