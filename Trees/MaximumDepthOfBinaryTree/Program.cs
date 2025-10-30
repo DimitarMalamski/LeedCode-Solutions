0874856f076242015df8148ad919cd185f934075
DimitarMalamski <mitemalamata@gmail.com>
2025-10-30 18:00:10 +0100

    add solution for Maximum Depth of Binary Tree ( Trees )

---
+++ Trees/MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿// Problem 104. Maximum Depth of Binary Tree
+// Link https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+// Difficulty: Easy
+
+class Solution
+{
+   static void Main(string[] args)
+   {
+      Solution sol = new Solution();
+
+      TreeNode root = new TreeNode(3);
+      root.left = new TreeNode(9);
+      root.right = new TreeNode(20);
+
+      root.right.left = new TreeNode(15);
+      root.right.right = new TreeNode(7);
+
+      int output = sol.MaxDepth(root);
+      Console.WriteLine(output); 
+   }
+   public int MaxDepth(TreeNode root)
+   {
+      if (root == null) return 0;
+
+      int leftDepth = MaxDepth(root.left);
+      int rightDepth = MaxDepth(root.right);
+
+      return Math.Max(leftDepth, rightDepth) + 1;
+   }
+
+   public class TreeNode
+   {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+      {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
+}
+
