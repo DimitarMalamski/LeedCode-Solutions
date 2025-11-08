33a60a324fce56ed0e649556d71b127b552d444b
DimitarMalamski <mitemalamata@gmail.com>
2025-11-08 23:38:54 +0100

    add solution for Find Numbers with Even Number of Digits ( Arrays )

---
+++ Arrays/FindNumbersWithEvenNumberOfDigits/Program.cs
@@ -0,0 +1,37 @@
+﻿// Problem 1295. Find Numbers with Even Number of Digits
+// Link https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 12, 345, 2, 6, 7896 };
+      int output = FindNumbers(nums);
+      Console.WriteLine(output);
+   }
+   static int FindNumbers(int[] nums)
+   {
+      int count = 0;
+      foreach (int num in nums)
+      {
+         if (FindDigits(num) % 2 == 0)
+         {
+            count++;
+         }
+      }
+
+      return count;
+   }
+
+   private static int FindDigits(int number)
+   {
+      int count = 0;
+      while (number != 0)
+      {
+         number /= 10;
+         count++;
+      }
+
+      return count;
+   }
+}
