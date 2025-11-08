95981e1e6c1ebe95c8c5ce7d0d8756aa58ce64d2
DimitarMalamski <mitemalamata@gmail.com>
2025-11-08 23:31:30 +0100

    add solution for Remove Element ( Arrays )

---
+++ Arrays/RemoveElement/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem 27. Remove Element
+// Link https://leetcode.com/problems/remove-element/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 3, 2, 2, 3 };
+      int val = 3;
+      int output = RemoveElement(nums, val);
+      Console.WriteLine(output);
+   }
+   static int RemoveElement(int[] nums, int val)
+   {
+      int write = 0;
+
+      for (int read = 0; read < nums.Length; read++)
+      {
+         if (nums[read] != val)
+         {
+            nums[write] = nums[read];
+            write++;
+         }
+      }
+
+      return write;
+   }
+}
