a39243331d767cfa518b082d997d2e9d12b74fb0
DimitarMalamski <mitemalamata@gmail.com>
2025-11-12 13:45:17 +0100

    add solution for Contains Duplicate II ( Arrays )

---
+++ Arrays/ContainsDuplicateII/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem 219. Contains Duplicate II
+// Link https://leetcode.com/problems/contains-duplicate-ii/description/?envType=problem-list-v2&envId=hash-table
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 1, 2, 3, 1 };
+      int k = 3;
+      bool output = ContainsNearbyDuplicate(nums, k);
+      Console.WriteLine(output);
+   }
+   public static bool ContainsNearbyDuplicate(int[] nums, int k)
+   {
+      HashSet<int> window = new HashSet<int>();
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+         if (window.Contains(nums[i])) return true;
+
+         window.Add(nums[i]);
+
+         if (window.Count > k)
+         {
+            window.Remove(nums[i - k]);
+         }
+      }
+
+      return false;
+   }
+}
