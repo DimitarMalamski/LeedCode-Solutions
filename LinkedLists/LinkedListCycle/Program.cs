d4562a40a4ebed93adc40cf60c4df7d682e97e39
DimitarMalamski <mitemalamata@gmail.com>
2025-11-12 12:59:35 +0100

    add solution for Linked List Cycle ( Linked Lists )

---
+++ LinkedLists/LinkedListCycle/Program.cs
@@ -0,0 +1,43 @@
+﻿// Problem 141. Linked List Cycle
+// Link https://leetcode.com/problems/linked-list-cycle/description/?envType=problem-list-v2&envId=hash-table
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      ListNode head = new ListNode(3);
+      head.next = new ListNode(2);
+      head.next.next = new ListNode(0);
+      head.next.next.next = new ListNode(-4);
+      head.next.next.next.next = new ListNode(10);
+
+      bool result = HasCycle(head);
+      Console.WriteLine(result);
+   }
+
+   public class ListNode
+   {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+         this.val = val;
+         this.next = next;
+      }
+   }
+
+   public static bool HasCycle(ListNode head)
+   {
+      ListNode fast = head;
+
+      while (fast != null && fast.next != null)
+      {
+         head = head.next;
+         fast = fast.next.next;
+
+         if (head == fast) return true;
+      }
+
+      return false;
+   }
+}
