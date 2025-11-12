13aac17ed05d35241f4a4b6713765d5824f67f81
DimitarMalamski <mitemalamata@gmail.com>
2025-11-12 13:34:51 +0100

    optimize solution for Majority Element ( Arrays )

--- Arrays/MajorityElement/Program.cs
+++ Arrays/MajorityElement/Program.cs
@@ -13,19 +13,40 @@ class Solution
    {
       Dictionary<int, int> numbers = new Dictionary<int, int>();
 
-      foreach (int num in nums) {
-         if (numbers.ContainsKey(num)) {
-               numbers[num]++;
+      foreach (int num in nums)
+      {
+         if (numbers.ContainsKey(num))
+         {
+            numbers[num]++;
          }
-         else {
-               numbers[num] = 1;
+         else
+         {
+            numbers[num] = 1;
          }
       }
 
-      foreach(var key in numbers) {
+      foreach (var key in numbers)
+      {
          if (key.Value > nums.Length / 2) return key.Key;
       }
 
       return 0;
+   }
+   public static int MajorityElementOptimized(int[] nums)
+   {
+      int count = 0;
+      int condidate = 0;
+
+      foreach (int num in nums)
+      {
+         if (count == 0)
+         {
+            condidate = num;
+         }
+
+         count += (num == condidate) ? 1 : -1;
+      }
+
+      return condidate;
    }
 }
