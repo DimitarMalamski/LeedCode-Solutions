5ea223755a46877eabe12869aeeba0c33770a725
DimitarMalamski <mitemalamata@gmail.com>
2025-11-12 13:32:00 +0100

    add solution for Majority Element ( Arrays )

---
+++ Arrays/MajorityElement/Program.cs
@@ -0,0 +1,31 @@
+﻿// Problem 169. Majority Element
+// Link https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=hash-table
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+      int output = MajorityElement(nums);
+      Console.WriteLine(output);
+   }
+   public static int MajorityElement(int[] nums)
+   {
+      Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+      foreach (int num in nums) {
+         if (numbers.ContainsKey(num)) {
+               numbers[num]++;
+         }
+         else {
+               numbers[num] = 1;
+         }
+      }
+
+      foreach(var key in numbers) {
+         if (key.Value > nums.Length / 2) return key.Key;
+      }
+
+      return 0;
+   }
+}
