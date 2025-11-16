e8f737dccde1e844890f8dc2389ffd89f77e4a92
DimitarMalamski <mitemalamata@gmail.com>
2025-11-16 20:10:02 +0100

    add solution for Ugly Number ( Math )

---
+++ Math/UglyNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem 263. Ugly Number
+// Link https://leetcode.com/problems/ugly-number/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 6;
+      bool output = IsUgly(n);
+      Console.WriteLine(output);
+   }
+   static bool IsUgly(int n)
+   {
+      if (n <= 0) return false;
+
+      int[] primes = { 2, 3, 5 };
+
+      foreach (int p in primes)
+      {
+         while (n % p == 0)
+         {
+            n /= p;
+         }
+      }
+
+      return n == 1;
+   }
+}
