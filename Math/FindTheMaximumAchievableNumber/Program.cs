8982e2d6ddaf7b4bbdcb8688a988597ebf0ee0d2
DimitarMalamski <mitemalamata@gmail.com>
2025-11-20 16:24:34 +0100

    add solution for Find the Maximum Achievable Number ( Math )

---
+++ Math/FindTheMaximumAchievableNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// Problem 2769. Find the Maximum Achievable Number
+// Link https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int num = 4;
+      int t = 1;
+      int output = TheMaximumAchievableX(num, t);
+      Console.WriteLine(output);
+   }
+
+   static int TheMaximumAchievableX(int num, int t)
+   {
+      return num + 2 * t;
+   }
+}
