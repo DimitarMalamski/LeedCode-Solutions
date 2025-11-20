3cf3c2348f800aa7c9c17b2e5170c76199ae997d
DimitarMalamski <mitemalamata@gmail.com>
2025-11-20 16:40:44 +0100

    add solution for Restore Finishing Order ( Arrays )

---
+++ Arrays/RestoreFinishingOrder/Program.cs
@@ -0,0 +1,29 @@
+﻿// Problem 3668. Restore Finishing Order
+// Link https://leetcode.com/problems/restore-finishing-order/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] arags)
+   {
+      int[] order = { 3, 1, 2, 5, 4 };
+      int[] friends = { 1, 3, 4 };
+      int[] output = RecoverOrder(order, friends);
+      Console.WriteLine(string.Join(", ", output));
+   }
+
+   static int[] RecoverOrder(int[] order, int[] friends)
+   {
+      HashSet<int> friendSet = new HashSet<int>(friends);
+      List<int> result = new List<int>();
+
+      foreach (int id in order)
+      {
+         if (friendSet.Contains(id))
+         {
+            result.Add(id);
+         }
+      }
+
+      return result.ToArray();
+   }
+}
