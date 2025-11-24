59a433e3fed723563a805acd9f7ace5b00a53277
DimitarMalamski <mitemalamata@gmail.com>
2025-11-24 10:05:10 +0100

    Find Minimum Operations to Make All Elements Divisible by Three ( Arrays )

---
+++ Arrays/FindMinimumOperationsToMakeAllElementsDivisibleByThree/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 3190. Find Minimum Operations to Make All Elements Divisible by Three
+// Link https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 1, 2, 3, 4 };
+      int output = MinimumOperations(nums);
+      Console.WriteLine(output);
+   }
+   static int MinimumOperations(int[] nums)
+   {
+      int count = 0;
+
+      foreach (int num in nums)
+      {
+         if (num % 3 != 0) count++;
+      }
+
+      return count;
+   }
+}
