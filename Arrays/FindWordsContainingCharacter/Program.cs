8821824b1925dd1200b44730c2be89e85b9a8830
DimitarMalamski <mitemalamata@gmail.com>
2025-11-24 10:25:50 +0100

    add solution for Find Words Containing Character ( Arrays )

---
+++ Arrays/FindWordsContainingCharacter/Program.cs
@@ -0,0 +1,34 @@
+﻿// Problem 2942. Find Words Containing Character
+// Link https://leetcode.com/problems/find-words-containing-character/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string[] words = { "leet", "code" };
+      char x = 'e';
+      List<int> output = FindWordsContaining(words, x);
+      Console.WriteLine(string.Join(",", output));
+   }
+   static List<int> FindWordsContaining(string[] words, char x)
+   {
+      List<int> result = new List<int>();
+
+      for (int i = 0; i < words.Length; i++)
+      {
+
+         char[] wordChar = words[i].ToCharArray();
+
+         foreach (char letter in wordChar)
+         {
+            if (letter == x)
+            {
+               result.Add(i);
+               break;
+            }
+         }
+      }
+
+      return result;
+   }
+}
