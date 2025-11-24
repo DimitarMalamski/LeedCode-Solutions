17899c77e20daea785b506053464fe0c214c96df
DimitarMalamski <mitemalamata@gmail.com>
2025-11-24 10:34:00 +0100

    add solution for Convert the Temperature ( Math )

---
+++ Math/ConvertTheTemperature/Program.cs
@@ -0,0 +1,19 @@
+﻿// Problem 2469. Convert the Temperature
+// Link https://leetcode.com/problems/convert-the-temperature/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      double celsius = 36.50;
+      double[] output = ConvertTemperature(celsius);
+      Console.WriteLine(string.Join(", ", output));
+   }
+   static double[] ConvertTemperature(double celsius)
+   {
+      double[] result = new double[2];
+      result[0] = celsius + 273.15;
+      result[1] = celsius * 1.80 + 32.00;
+      return result; 
+   }
+}
