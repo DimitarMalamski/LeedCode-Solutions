073c658cce43403545f49f97dc11be77a98b21a9
DimitarMalamski <mitemalamata@gmail.com>
2025-12-01 19:23:37 +0100

    add solution for Split a String in Balanced Strings ( Strings )

---
+++ Strings/SplitAStringInBalancedStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿// Problem 1221. Split a String in Balanced Strings
+// Link https://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string s = "RLRRLLRLRL";
+      int output = BalancedStringSplit(s);
+      Console.WriteLine(output);
+   }
+   static int BalancedStringSplit(string s)
+   {
+      int counter = 0;
+      int result = 0;
+
+      foreach (char letter in s)
+      {
+         switch (letter)
+         {
+            case 'R':
+               counter++;
+               break;
+            case 'L':
+               counter--;
+               break;
+         }
+
+         if (counter == 0) result++;
+      }
+
+      return result;
+   }
+}
