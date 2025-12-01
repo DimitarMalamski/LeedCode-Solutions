a73db694c3849ae1d33742854bb2abcdcb1ad9ad
DimitarMalamski <mitemalamata@gmail.com>
2025-12-01 20:14:39 +0100

    add solution for Count the Number of Consistent Strings ( Arrays )

---
+++ Arrays/CountTheNumberOfConsistentStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿// Problem 1684. Count the Number of Consistent Strings
+// Link https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string allowed = "ab";
+      string[] words = { "ad", "bd", "aaab", "baa", "badab" };
+      int output = CountConsistentStrings(allowed, words);
+      Console.WriteLine(output);
+   }
+   static int CountConsistentStrings(string allowed, string[] words)
+   {
+      int count = 0;
+
+      foreach (string word in words)
+      {
+         bool consistent = true;
+
+         foreach (char c in word)
+         {
+            if (!allowed.Contains(c))
+            {
+               consistent = false;
+               break;
+            }
+         }
+
+         if (consistent) count++;
+      }
+
+      return count;
+   }
+}
