3099f9ce044d90c9527e2e9b618a2ae5310a9e83
DimitarMalamski <mitemalamata@gmail.com>
2025-12-01 20:47:18 +0100

    add solution for Maximum 69 Number ( Math )

---
+++ Math/Maximum69Number/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 1323. Maximum 69 Number
+// Link https://leetcode.com/problems/maximum-69-number/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int num = 9669;
+      int output = Maximum69Number(num);
+      Console.WriteLine(output);
+   }
+   static int Maximum69Number(int num)
+   {
+      char[] digits = num.ToString().ToCharArray();
+
+      for (int i = 0; i < digits.Length; i++)
+      {
+         if (digits[i] == '6')
+         {
+            digits[i] = '9';
+            break;
+         }
+      }
+
+      return int.Parse(digits);
+   }
+}
