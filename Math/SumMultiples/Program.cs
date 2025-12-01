aadbd97359a735f89e73e6597a82e425599f579f
DimitarMalamski <mitemalamata@gmail.com>
2025-12-01 20:29:27 +0100

    add solution for Sum Multiples ( Math )

---
+++ Math/SumMultiples/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 2652. Sum Multiples
+// Link https://leetcode.com/problems/sum-multiples/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 7;
+      int output = SumOfMultiples(n);
+      Console.WriteLine(output);
+   }
+   static int SumOfMultiples(int n)
+   {
+      int result = 0;
+
+      for (int i = 1; i <= n; i++)
+      {
+         if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) result += i;
+      }
+
+      return result;
+   }
+}
