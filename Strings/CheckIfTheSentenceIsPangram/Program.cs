4da9a8ca60c73848106436e2cc747c7c2f06bfef
DimitarMalamski <mitemalamata@gmail.com>
2025-12-03 00:58:47 +0100

    add solution for Check if the Sentence Is Pangram ( Strings )

---
+++ Strings/CheckIfTheSentenceIsPangram/Program.cs
@@ -0,0 +1,25 @@
+﻿// Problem 1832. Check if the Sentence Is Pangram
+// Link https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string sentence = "thequickbrownfoxjumpsoverthelazydog";
+      bool output = CheckIfPangram(sentence);
+      Console.WriteLine(output);
+   }
+   static bool CheckIfPangram(string sentence)
+   {
+      string filtered = sentence.ToLower();
+
+      for (int i = 0; i < 26; i++)
+      {
+         char letter = (char)('a' + i);
+
+         if (!filtered.Contains(letter)) return false;
+      }
+
+      return true;
+   }
+}
