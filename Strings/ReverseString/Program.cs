54ed43916d880ced0f8291b51318d72427e18b20
DimitarMalamski <mitemalamata@gmail.com>
2025-12-08 15:48:00 +0100

    add solution for Reverse String ( Strings )

---
+++ Strings/ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿// Problem 344. Reverse String
+// Link https://leetcode.com/problems/reverse-string/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      char[] s = { 'h', 'e', 'l', 'l', 'o' };
+      ReverseString(s);
+      Console.WriteLine(string.Join(",", s));
+   }
+   static void ReverseString(char[] s)
+   {
+      int p1 = 0;
+      int p2 = s.Length - 1;
+
+      while (p2 > p1)
+      {
+         char temp = s[p1];
+         s[p1] = s[p2];
+         s[p2] = temp;
+
+         p1++;
+         p2--;
+      }
+   }
+}
