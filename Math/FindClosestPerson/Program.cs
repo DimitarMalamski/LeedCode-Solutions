117ba4b51c34df34ad46baed7dde8017cd1fae05
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 14:25:53 +0100

    add solution for Find Closest Person ( Math )

---
+++ Math/FindClosestPerson/Program.cs
@@ -0,0 +1,20 @@
+﻿// Problem 3516. Find Closest Person
+// Link https://leetcode.com/problems/find-closest-person/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int x = 2, y = 7, z = 4;
+      int output = FindClosest(x, y, z);
+      Console.WriteLine(output);
+   }
+   static int FindClosest(int x, int y, int z)
+   {
+      return Math.Abs(x - z) < Math.Abs(y - z)
+         ? 1
+         : (Math.Abs(x - z) == Math.Abs(y - z)
+            ? 0
+            : 2);
+   }
+}
