6dadd637a687c4e8b8709e14a5774dbd36ef3ee6
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 14:33:46 +0100

    add solution for Convert Date to Binary ( Math )

---
+++ Math/ConvertDateToBinary/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem 3280. Convert Date to Binary
+// Link https://leetcode.com/problems/convert-date-to-binary/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string date = "2080-02-29";
+      string output = ConvertDateToBinary(date);
+      Console.WriteLine(output);
+   }
+   static string ConvertDateToBinary(string date)
+   {
+      string[] parts = date.Split('-');
+
+      int year = int.Parse(parts[0]);
+      int month = int.Parse(parts[1]);
+      int day = int.Parse(parts[2]);
+
+      string binaryYear = Convert.ToString(year, 2);
+      string binaryMonth = Convert.ToString(month, 2);
+      string binaryDay = Convert.ToString(day, 2);
+
+      return $"{binaryYear}-{binaryMonth}-{binaryDay}";
+   }
+}
