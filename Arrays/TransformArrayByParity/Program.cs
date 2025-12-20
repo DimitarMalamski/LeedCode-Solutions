39bbe351d351858828df86ca6548575180c05ea5
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 14:13:29 +0100

    add solution for Transform Array by Parity ( Arrays )

---
+++ Arrays/TransformArrayByParity/Program.cs
@@ -0,0 +1,33 @@
+﻿// Problem 3467. Transform Array by Parity
+// Link https://leetcode.com/problems/transform-array-by-parity/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 4, 3, 2, 1 };
+      int[] output = TransformArray(nums);
+      Console.WriteLine(string.Join(",", output));
+   }
+   static int[] TransformArray(int[] nums)
+   {
+      int n = nums.Length;
+      int[] result = new int[n];
+
+      for (int i = 0; i < n; i++)
+      {
+         if (nums[i] % 2 == 0)
+         {
+            result[i] = 0;
+         }
+         else
+         {
+            result[i] = 1;
+         }
+      }
+
+      Array.Sort(result);
+
+      return result;
+   }
+}
