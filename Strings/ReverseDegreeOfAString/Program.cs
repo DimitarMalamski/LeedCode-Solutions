b9c890dbc7e8ccf56981297b62dda35a029a1c69
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 14:41:10 +0100

    add solution for Reverse Degree of a String ( Strings )

---
+++ Strings/ReverseDegreeOfAString/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem 3498. Reverse Degree of a String
+// Link https://leetcode.com/problems/reverse-degree-of-a-string/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string s = "zaza";
+      int output = ReverseDegree(s);
+      Console.WriteLine(output);
+   }
+   static int ReverseDegree(string s)
+   {
+      int sum = 0;
+
+      for (int i = 0; i < s.Length; i++)
+      {
+         int reverse = 'z' - s[i] + 1;
+         int stringPos = i + 1;
+
+         sum += reverse * stringPos;
+      }
+
+      return sum;
+   }
+}
