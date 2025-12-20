4e8f81afdd9ea2540dd58db55354321672ca0f06
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 14:49:58 +0100

    add solution for Smallest Even Multiple ( Math )

---
+++ Math/SmallestEvenMultiple/Program.cs
@@ -0,0 +1,14 @@
+﻿// Problem 2413. Smallest Even Multiple
+// Link https://leetcode.com/problems/smallest-even-multiple/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int n = 5;
+      int output = SmallestEvenMultiple(n);
+      Console.WriteLine(output);
+   }
+   static int SmallestEvenMultiple(int n)
+      => n % 2 == 0 ? n : n * 2;
+}
