f5b0af44c2369968fbd9b5e05f3f4668d0c18cc6
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 13:57:45 +0100

    add solution for Defanging an IP Address ( Strings )

---
+++ Strings/DefangingAnIPAddress/Program.cs
@@ -0,0 +1,14 @@
+﻿// Problem 1108. Defanging an IP Address
+// Link https://leetcode.com/problems/defanging-an-ip-address/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      string address = "1.1.1.1";
+      string output = DefangIPaddr(address);
+      Console.WriteLine(output);
+   }
+   static string DefangIPaddr(string address)
+      => address.Replace(".", "[.]");
+}
