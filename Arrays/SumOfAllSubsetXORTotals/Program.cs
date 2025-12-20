eb6cd4705579e204d55c20cbe1e1f5a17cb44f52
DimitarMalamski <mitemalamata@gmail.com>
2025-12-20 13:40:34 +0100

    add solution for Sum of All Subset XOR Totals ( Arrays )

---
+++ Arrays/SumOfAllSubsetXORTotals/Program.cs
@@ -0,0 +1,23 @@
+﻿// Problem 1863. Sum of All Subset XOR Totals
+// Link https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/
+// Difficulty: Easy
+class Solution
+{
+   static void Main(string[] args)
+   {
+      int[] nums = { 1, 3 };
+      int output = SubsetXORSum(nums);
+      Console.WriteLine(output);
+   }
+   static int SubsetXORSum(int[] nums)
+   {
+      int or = 0;
+
+      foreach (int num in nums)
+      {
+         or |= num;
+      }
+
+      return or * (1 << (nums.Length - 1));
+   }
+}
